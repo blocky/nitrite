cde9b5d05d07b48b046ff597836af7749a99fe00
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2023-06-14 20:01:59 -0400

    Initial commit of empty test file

---
+++ nitrite_test.go
@@ -0,0 +1,47 @@
+package nitrite_test
+
+import "testing"
+
+func TestCheckECDSASignature(t *testing.T) {
+
+}
+
+func TestExtractAttestationDoc(t *testing.T) {
+
+}
+
+func TestExtractCertificates(t *testing.T) {
+
+}
+
+func TestExtractCoseHeader(t *testing.T) {
+
+}
+
+func TestExtractCosePayload(t *testing.T) {
+
+}
+
+func TestVerifyAttestationDoc(t *testing.T) {
+
+}
+
+func TestVerifyCertificates(t *testing.T) {
+
+}
+
+func TestVerifyCoseHeader(t *testing.T) {
+
+}
+
+func TestVerifyCosePayload(t *testing.T) {
+
+}
+
+func TestVerifyCoseSign1(t *testing.T) {
+
+}
+
+func TestVerify(t *testing.T) {
+
+}
