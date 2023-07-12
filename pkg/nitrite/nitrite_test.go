88994e10db7513de5845a3fb6c8a35ff9b7b2a55
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2023-07-12 10:48:31 -0400

    Add unit testing for nitrite module

--- pkg/nitrite/nitrite_test.go
+++ pkg/nitrite/nitrite_test.go
@@ -1,7 +1,128 @@
 package nitrite_test
 
-import "testing"
+import (
+	_ "embed"
+	"encoding/base64"
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/blocky/nitrite/pkg/attestation"
+	"github.com/blocky/nitrite/pkg/nitrite"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+//go:embed test/nitro-stag-attestation.txt
+var nitroStagAttestBase64 string
+
+//go:embed test/nitro-stag-attestation-doc.json
+var nitroStagAttestDocJSON string
+
+func makeAttestationDocFromJSON(
+	attestationDocJSON []byte,
+) (*attestation.Document, error) {
+	doc := attestation.Document{}
+	err := json.Unmarshal(attestationDocJSON, &doc)
+	if err != nil {
+		return nil, err
+	}
+	if doc.UserData == nil {
+		doc.UserData = make([]byte, 0)
+	}
+	if doc.Nonce == nil {
+		doc.Nonce = make([]byte, 0)
+	}
+	return &doc, nil
+}
+
+func makeNitroStagVerifyOptions(
+	currentTime time.Time,
+) nitrite.VerifyOptions {
+	return nitrite.VerifyOptions{
+		Roots:               nil,
+		CurrentTime:         currentTime,
+		AllowSelfSignedCert: false,
+	}
+}
 
 func TestNitrite_Verify(t *testing.T) {
+	attestBytes, err := base64.StdEncoding.DecodeString(nitroStagAttestBase64)
+	require.NoError(t, err)
 
+	t.Run("happy path", func(t *testing.T) {
+		doc, err := makeAttestationDocFromJSON([]byte(nitroStagAttestDocJSON))
+		require.NoError(t, err)
+
+		validTime := time.Date(2023, time.July, 10, 15, 22, 53, 0, time.UTC)
+		verifyOptions := makeNitroStagVerifyOptions(validTime)
+
+		res, err := nitrite.Verify(attestBytes, verifyOptions)
+		assert.NoError(t, err)
+		assert.Equal(t, doc, res.Document)
+	})
+
+	t.Run("invalid time", func(t *testing.T) {
+		invalidTime := time.Date(2022, time.July, 10, 15, 22, 53, 0, time.UTC)
+		verifyOptions := makeNitroStagVerifyOptions(invalidTime)
+
+		res, err := nitrite.Verify(attestBytes, verifyOptions)
+		assert.ErrorContains(t, err, "is not yet valid")
+		assert.Nil(t, res)
+	})
+
+	t.Run("expired time", func(t *testing.T) {
+		expiredTime := time.Date(2024, time.July, 10, 15, 22, 53, 0, time.UTC)
+		verifyOptions := makeNitroStagVerifyOptions(expiredTime)
+
+		res, err := nitrite.Verify(attestBytes, verifyOptions)
+		assert.ErrorContains(t, err, "certificate has expired")
+		assert.Nil(t, res)
+	})
+
+	t.Run("nitro self-signed mismatch", func(t *testing.T) {
+		validTime := time.Date(2023, time.July, 10, 15, 22, 53, 0, time.UTC)
+		verifyOptions := makeNitroStagVerifyOptions(validTime)
+		verifyOptions.AllowSelfSignedCert = true
+
+		res, err := nitrite.Verify(attestBytes, verifyOptions)
+		assert.ErrorContains(t, err, "certificate signed by unknown authority")
+		assert.Nil(t, res)
+	})
+}
+
+func FuzzNitrite_Verify(f *testing.F) {
+	attestBytes, err := base64.StdEncoding.DecodeString(nitroStagAttestBase64)
+	require.NoError(f, err)
+
+	badBytes1 := attestBytes[1:]
+	badBytes2 := attestBytes[:len(attestBytes)-1]
+	badBytes3 := attestBytes
+	badBytes4 := attestBytes
+	badBytes5 := attestBytes
+	badBytes3[0]++
+	badBytes4[100]++
+	badBytes5[1000]++
+
+	tests := []struct {
+		data []byte
+	}{
+		{data: badBytes1},
+		{data: badBytes2},
+		{data: badBytes3},
+		{data: badBytes4},
+		{data: badBytes5},
+	}
+	for _, test := range tests {
+		f.Add(test.data)
+	}
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		validTime := time.Date(2023, time.July, 10, 15, 22, 53, 0, time.UTC)
+		verifyOptions := makeNitroStagVerifyOptions(validTime)
+
+		res, err := nitrite.Verify(data, verifyOptions)
+		assert.Error(t, err)
+		assert.Nil(t, res)
+	})
 }
