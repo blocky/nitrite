503546282ea8ba39968fc95eb4ccf22e2d9f7267
Mike Wittie <mwittie@blocky.rocks>
2024-09-12 18:32:47 -0600

    Split out and clarify integration tests

--- test/integration/certs_test.go
+++ test/integration/certs_test.go
@@ -9,21 +9,44 @@ import (
 	"github.com/blocky/nitrite/internal"
 )
 
+func TestFetchingNitroCertProvider_Roots(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		if testing.Short() {
+			t.SkipNow()
+		}
+
+		// given
+		cp := internal.NewFetchingNitroCertProvider()
+
+		// when
+		got, err := cp.Roots()
+
+		// then
+		require.NoError(t, err)
+		assert.NotEmpty(t, got)
+	})
+}
+
+// TestEmbeddedAndFetchedCertsEqual tests that the certs fetched from AWS
+// and the certs embedded in the repository are the same. If they are not,
+// it may be possible the AWS has updated the Nitro Enclaves root certs.
 func TestEmbeddedAndFetchedCertsEqual(t *testing.T) {
-	if testing.Short() {
-		t.SkipNow()
-	}
+	t.Run("happy path", func(t *testing.T) {
+		if testing.Short() {
+			t.SkipNow()
+		}
 
-	// given
-	embeddedCP := internal.NewNitroCertProvider()
-	fetchingCP := internal.NewFetchingNitroCertProvider()
+		// given
+		embeddedCP := internal.NewNitroCertProvider()
+		fetchingCP := internal.NewFetchingNitroCertProvider()
 
-	// when
-	gotEmbeddedRoots, err := embeddedCP.Roots()
-	gotFetchedRoots, err2 := fetchingCP.Roots()
+		// when
+		gotEmbeddedRoots, err := embeddedCP.Roots()
+		gotFetchedRoots, err2 := fetchingCP.Roots()
 
-	// then
-	require.NoError(t, err)
-	require.NoError(t, err2)
-	assert.True(t, gotEmbeddedRoots.Equal(gotFetchedRoots))
+		// then
+		require.NoError(t, err)
+		require.NoError(t, err2)
+		assert.True(t, gotEmbeddedRoots.Equal(gotFetchedRoots))
+	})
 }
